4ca6c1881a3788d06788caae93ddef30ee7a785b
Trevor <25110388+TrevTV@users.noreply.github.com>
2023-06-13 20:32:06 -0400

    Remove debug libmain compilation stuff

--- libmain/build.bat
+++ libmain/build.bat
@@ -1,4 +1,2 @@
-cargo ndk -t arm64-v8a -o ./jniLibs build 
-rem --release
-copy F:\Projects\GitHub\MelonLoader\libmain\jniLibs\arm64-v8a\libmain.so C:\Users\trevo\Desktop\com.SirCoolness.UnityTestModdingTarget\lib\arm64-v8a
+cargo ndk -t arm64-v8a -o ./jniLibs build --release
 pause
