6114d6cf05f6abfe5454123531d3a9e9405f660c
Trevor <25110388+TrevTV@users.noreply.github.com>
2023-08-29 19:24:36 -0400

    Disable AssemblyLoader logs
    There may or not be a major issue where Quicklogs can randomly override unrelated variables.

--- Bootstrap/Utils/AssemblyLoader.cpp
+++ Bootstrap/Utils/AssemblyLoader.cpp
@@ -5,13 +5,9 @@
 #include <sys/stat.h>
 #include <sys/sendfile.h>
 #include <dlfcn.h>
-
-#include <iostream>
-#include <filesystem>
 #include <dirent.h>
 
 #include "../Managers/AndroidData.h"
-#include "./Console/Logger.h"
 
 std::string AssemblyLoader::AssemblyTempPath;
 
@@ -22,23 +18,23 @@ bool AssemblyLoader::Initialize() {
 
     AssemblyTempPath = modTempPath;
 
-    Logger::QuickLogf(LogType::Debug, "AssemblyLoader Temp: %s", AssemblyTempPath.c_str());
+    //Logger::QuickLogf(LogType::Debug, "AssemblyLoader Temp: %s", AssemblyTempPath.c_str());
 
     // copied from
     // https://github.com/sc2ad/QuestLoader/blob/master/libmodloader/src/modloader.cpp#L495
     struct dirent* dp;
     DIR* dir = opendir(AssemblyTempPath.c_str());
     if (dir == nullptr) {
-        Logger::QuickLogf(LogType::Error, "Could not clear temp dir %s: null dir! errno: %i, msg: %s!", modTempPath.c_str(), errno, strerror(errno));
+        //Logger::QuickLogf(LogType::Error, "Could not clear temp dir %s: null dir! errno: %i, msg: %s!", modTempPath.c_str(), errno, strerror(errno));
     } else {
         while ((dp = readdir(dir)) != NULL) {
             if (strlen(dp->d_name) > 3 && !strcmp(dp->d_name + strlen(dp->d_name) - 3, ".so")) {
                 auto str = modTempPath + dp->d_name;
                 // Delete all .so files in our modTempPath
                 if (unlink(str.c_str())) {
-                    Logger::QuickLogf(LogType::Warning, "Failed to delete: %s errno: %i, msg: %s", str.c_str(), errno, strerror(errno));
+                    //Logger::QuickLogf(LogType::Warning, "Failed to delete: %s errno: %i, msg: %s", str.c_str(), errno, strerror(errno));
                 } else {
-                    Logger::QuickLogf(LogType::Debug, "Deleted: %s", str.c_str());
+                    //Logger::QuickLogf(LogType::Debug, "Deleted: %s", str.c_str());
                 }
             }
         }
